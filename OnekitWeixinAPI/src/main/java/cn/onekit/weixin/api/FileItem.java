9e0ae1373858d803d1581e2f20526df2dd5f1962
15802303637 <>
2020-12-18 22:07:14 +0800

    .

--- OnekitWeixinAPI/src/main/java/cn/onekit/weixin/api/FileItem.java
+++ OnekitWeixinAPI/src/main/java/cn/onekit/weixin/api/FileItem.java
@@ -3,13 +3,9 @@ package cn.onekit.weixin.api;
 import cn.onekit.JsAny;
 
 
-public class FileItem implements JsAny {
+public class FileItem extends JsClass {
     public String filePath;
     public int createTime;
     public int size;
 
-    @Override
-    public JsAny ToString() {
-        return null;
-    }
 }
